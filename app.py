3cdacdf20bff365099f7eba0b0d0b2626cf6eda6
Linoy Tsaban <LinoyTsaban@users.noreply.huggingface.co>
2023-05-24 09:28:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import gradio as gr
+import torch
+import requests
+from io import BytesIO
+from diffusers import StableDiffusionPipeline
+from diffusers import DDIMScheduler
+
 
 def greet(name):
     return "Hello " + name + "!!"

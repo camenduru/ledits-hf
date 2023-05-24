b2a1da098194cdf1547eff14d3ed0b48119d5740
Linoy Tsaban <LinoyTsaban@users.noreply.huggingface.co>
2023-05-24 09:36:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import requests
 from io import BytesIO
 from diffusers import StableDiffusionPipeline
 from diffusers import DDIMScheduler
-
+from .utils import hi 
 
 def greet(name):
-    return "Hello " + name + "!!"
+    # return "Hello " + name + "!!"
+    return hi() +""+ name
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
